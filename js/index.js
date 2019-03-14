bb454b38979bd49e03f7f7dbd54b2d93f1f18de5
Santiago Iglesias <iglesias.arozena@gmail.com>
2019-03-14 23:45:58 +0100

    inicio juego con cuenta atrás

--- js/index.js
+++ js/index.js
@@ -2,9 +2,51 @@ window.onload = function() {
   var game = new Game("canvas");
   
   document.getElementById("startGame").addEventListener("click", function(){
-    game.start();
+    removeHome();
+    readyForPlay();
+    
+    
   });
+
+  function readyForPlay() {
+    var cuentaAtras = 3;
+    var ready = document.getElementById('counting');
+
+    var count = setInterval(function(){
+      if (cuentaAtras == 0) {
+        cuentaAtras = 'GO';
+        clearInterval(count);
+        callStart();
+        var stopCount = setInterval(function() {
+          document.getElementById('counting').remove();
+          clearInterval(stopCount);
+        },1000);
+      }
+      ready.innerHTML = cuentaAtras;
+      if (cuentaAtras != 'GO'){
+        cuentaAtras--;
+      }
+    }, 1000); 
+  }
+
+  function removeHome() {
+    document.getElementById('startGame').remove();
+    document.getElementById('gameTitle').remove();
+    //document.getElementById('counting').remove();
+    //callStart();
+  }
+  
+  function callStart() {
+    game.start();
+  }
+
 };
+//var para = document.createElement("p");
+//var node = document.createTextNode("This is new.");
+//para.appendChild(node);
+//
+//var element = document.getElementById("div1");
+//element.appendChild(para);
 
 /*
 window.onload = function() {
